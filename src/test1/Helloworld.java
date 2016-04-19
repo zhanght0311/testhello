48a42b23a70b872ff0bccd917a77b39979679da3
张海涛 <zhanghaitao0311@foxmail.com>
2016-04-19 19:34:32 +0800

    the first rebuide

--- src/test1/Helloworld.java
+++ src/test1/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world zht!");
+		System.out.println("hello world xiugai!");
 	}
 
 }
